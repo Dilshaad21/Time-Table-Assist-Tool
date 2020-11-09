c8ff3da31d4955f7adaa17dafd02f74dc3bf2125
Dilshaad21 <dilshaadmuthalif21@gmail.com>
2020-11-09 22:42:07 +0530

    Added comments in views.py

--- slot_decider/views.py
+++ slot_decider/views.py
@@ -4,16 +4,16 @@ from .func import slot_allot
 # Create your views here.
 
 def file_upload(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
+    if request.method == 'POST':    # POST method in the case of the files submission
+        form = UploadFileForm(request.POST, request.FILES)  
         if form.is_valid():
             # handle_uploaded_file(request.FILES['file'])
-            files = request.FILES.getlist('file_field')
+            files = request.FILES.getlist('file_field') # Files stored in a list format
             if(files[1].name=='class.csv'):
                 files[::-1]
             d = slot_allot(files[1], files[0])
-            slots = []
-            colors = {0:'red', 1:'green', 2:'blue', 3:'yellow', 4:'orange', 5:'violet', 6:'pink'}
+            slots = []  # This list will contain all the slot names that need to be passed to the templates
+            colors = {0:'red', 1:'green', 2:'blue', 3:'yellow', 4:'orange', 5:'violet', 6:'pink'} # Color coding for slots
             i = 0
             for x in d:
                 j = 0
@@ -26,11 +26,11 @@ def file_upload(request):
                 slots.append(dd)
                 i+=1
             print(slots)
-            context = {'slot0': slots[0], 'slot1': slots[1],'slot2': slots[2],'slot3': slots[3],'slot4': slots[4],}
-            return render(request,'time-table.html', context)
+            context = {'slot0': slots[0], 'slot1': slots[1],'slot2': slots[2],'slot3': slots[3],'slot4': slots[4],} # Each slot list passed for each day in a week
+            return render(request,'time-table.html', context) # render time-table for post request after file is uploaded
     else:
         form = UploadFileForm()
-    return render(request, 'form.html', {'form': form})
+    return render(request, 'form.html', {'form': form}) # render form.html when get request is done
 
 
 def time_table(request):
