064120a5207142f12f21c6c7766cb1b636dc78de
Dilshaad21 <dilshaadmuthalif21@gmail.com>
2020-11-16 19:31:34 +0530

    Slot-wise course list added

--- slot_decider/views.py
+++ slot_decider/views.py
@@ -3,34 +3,49 @@ from .forms import UploadFileForm
 from .func import slot_allot
 # Create your views here.
 
+
 def file_upload(request):
     if request.method == 'POST':    # POST method in the case of the files submission
-        form = UploadFileForm(request.POST, request.FILES)  
+        form = UploadFileForm(request.POST, request.FILES)
         if form.is_valid():
             # handle_uploaded_file(request.FILES['file'])
-            files = request.FILES.getlist('file_field') # Files stored in a list format
-            if(files[1].name=='class.csv'):
+            # Files stored in a list format
+            files = request.FILES.getlist('file_field')
+            if(files[1].name == 'class.csv'):
                 files[::-1]
             d = slot_allot(files[1], files[0])
             slots = []  # This list will contain all the slot names that need to be passed to the templates
-            colors = {0:'red', 1:'green', 2:'blue', 3:'yellow', 4:'orange', 5:'violet', 6:'pink'} # Color coding for slots
+            colors = {0: 'red', 1: 'green', 2: 'blue', 3: 'yellow',
+                      4: 'orange', 5: 'violet', 6: 'pink'}  # Color coding for slots
             i = 0
+            hh = 0
+            lunch = ['L','U','N','C','H']
             for x in d:
                 j = 0
                 dd = []
                 for xx in x:
-                    if(i == 0 and j==4):
-                        dd.append('L')
+                    if(j == 4):
+                        dd.append([lunch[hh], 'white'])
                     dd.append([chr(ord('A')+xx), colors[xx]])
-                    j+=1
+                    j += 1
                 slots.append(dd)
-                i+=1
+                i += 1
+                hh += 1
             print(slots)
-            context = {'slot0': slots[0], 'slot1': slots[1],'slot2': slots[2],'slot3': slots[3],'slot4': slots[4],} # Each slot list passed for each day in a week
-            return render(request,'time-table.html', context) # render time-table for post request after file is uploaded
+            # Each slot list passed for each day in a week
+            course_table = {'Slot A': ['c1', 'c2', 'c3'], 'Slot B': ['c1', 'c2', 'c3'], 'Slot C': ['c1', 'c2', 'c3'], 
+                            'Slot D': ['c1', 'c2', 'c3'], 'Slot E': ['c1', 'c2', 'c3'], 'Slot F': ['c1', 'c2', 'c3'],
+                            'Slot G': ['c1', 'c2', 'c3'], 'Slot H': ['c1', 'c2', 'c3']}
+            context = {'slot_table': {'Monday': slots[0], 'Tuesday': slots[1],
+                                      'Wednesday': slots[2], 'Thursday': slots[3], 'Friday': slots[4]},
+                         'course_table': course_table,
+                       }
+            # render time-table for post request after file is uploaded
+            return render(request, 'time-table.html', context)
     else:
         form = UploadFileForm()
-    return render(request, 'form.html', {'form': form}) # render form.html when get request is done
+    # render form.html when get request is done
+    return render(request, 'form.html', {'form': form})
 
 
 def time_table(request):
